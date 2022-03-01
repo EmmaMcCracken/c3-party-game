de774a6bb6179b2d48fb21df570df3571dfcd469
Emma McCracken <emma.mccracken14@gmail.com>
2022-03-01 17:05:21 +0000

    add timer of 5 seconds for each play

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 // PLAYERS is an array of strings where each string is a name of a player
-// Two times table game
+// Times table game, default times table is the 2 times table
 
 class Game {
-  constructor(players, timesTable = 2) {
+  constructor(players, timesTable = 2, timeAllowed = 5) {
     [this.next, this.deletePlayer] = generateCycler(players);
     this.currentPlayer = this.next();
     this.gameOver = false;
     this.timesTable = timesTable;
+    this.timeAllowed = timeAllowed;
+    this.startTime = new Date();
   }
 
   play(num) {
+    this.endTime = new Date();
+    const seconds = (this.endTime.getTime() - this.startTime.getTime()) / 1000;
+    this.startTime = new Date();
+
     if (this.gameOver) {
       return `${this.currentPlayer} has won!`;
     }
-    if (num % this.timesTable === 0) {
+    if (seconds <= 5 && num % this.timesTable === 0) {
       this.currentPlayer = this.next();
       return `It is ${this.currentPlayer}'s turn.`;
     }
