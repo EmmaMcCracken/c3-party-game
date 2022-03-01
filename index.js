85d43bdd7ef0472d515e3130f48c080a08ce86b7
Emma McCracken <emma.mccracken14@gmail.com>
2022-03-01 15:54:12 +0000

    add a message to notify of a winner

--- index.js
+++ index.js
@@ -1,40 +1,45 @@
 // PLAYERS is an array of strings where each string is a name of a player
 // Two times table game
-// TO-DO: return winning message
 
 class Game {
   constructor(players) {
-    this.players = players;
     [this.next, this.deletePlayer] = generateCycler(players);
     this.currentPlayer = this.next();
+    this.gameOver = false;
   }
 
   play(num) {
+    if (this.gameOver) {
+      return `${this.currentPlayer} has won!`;
+    }
     if (num % 2 === 0) {
       this.currentPlayer = this.next();
       return `It is ${this.currentPlayer}'s turn.`;
-    } else {
-      const deadPlayer = this.currentPlayer;
-      this.deletePlayer();
-      this.currentPlayer = this.next();
-      return `${deadPlayer} has lost the game. It is ${this.currentPlayer}'s turn`;
     }
+    const deadPlayer = this.currentPlayer;
+    this.gameOver = this.deletePlayer();
+    this.currentPlayer = this.next();
+    return this.gameOver
+      ? `${this.currentPlayer} has won!`
+      : `${deadPlayer} has lost the game. It is ${this.currentPlayer}'s turn`;
   }
 }
 
-function generateCycler(arr) {
+function generateCycler(players) {
   let ix = 0;
   let element;
 
   function next() {
-    element = arr[ix];
-    ix = (ix + 1) % arr.length;
+    element = players[ix];
+    ix = (ix + 1) % players.length;
     return element;
   }
 
   function deletePlayer() {
-    arr = arr.filter((e) => e !== element);
-    ix = (ix - 1) % arr.length;
+    players = players.filter((e) => e !== element);
+    ix = (ix - 1) % players.length;
+    const gameOver = players.length === 1;
+    return gameOver;
   }
   return [next, deletePlayer];
 }
