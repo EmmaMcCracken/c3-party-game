54f63c64fe779725e4d46b7cb3a29513705ea262
Emma McCracken <emma.mccracken14@gmail.com>
2022-03-01 12:42:38 +0000

    initial commit with 2 times table game

---
+++ index.js
@@ -0,0 +1,39 @@
+// PLAYERS is an array of strings where each string is a name of a player
+// Two times table game
+
+class Game {
+  constructor(players) {
+    this.players = players;
+    [this.next, this.deletePlayer] = generateCycler(players);
+    this.currentPlayer = this.next();
+  }
+
+  play(num) {
+    if (num % 2 === 0) {
+      this.currentPlayer = this.next();
+      return `It is ${this.currentPlayer}'s turn.`;
+    } else {
+      const deadPlayer = this.currentPlayer;
+      this.deletePlayer();
+      this.currentPlayer = this.next();
+      return `${deadPlayer} has lost the game. It is ${this.currentPlayer}'s turn`;
+    }
+  }
+}
+
+function generateCycler(arr) {
+  let ix = 0;
+  let element;
+
+  function next() {
+    element = arr[ix];
+    ix = (ix + 1) % arr.length;
+    return element;
+  }
+
+  function deletePlayer() {
+    arr = arr.filter((e) => e !== element);
+    ix = (ix - 1) % arr.length;
+  }
+  return [next, deletePlayer];
+}
