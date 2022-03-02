e114eec99710d2cfcb115d5c2fac91789804b8c4
Emma McCracken <emma.mccracken14@gmail.com>
2022-03-02 09:16:11 +0000

    game now asks for a variety of times tables

--- index.js
+++ index.js
@@ -2,33 +2,50 @@
 // Times table game, default times table is the 2 times table
 
 class Game {
-  constructor(players, timesTable = 2, timeAllowed = 5) {
+  constructor(
+    players,
+    timesTables = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    timeAllowed = 5
+  ) {
     [this.next, this.deletePlayer] = generateCycler(players);
     this.currentPlayer = this.next();
     this.gameOver = false;
-    this.timesTable = timesTable;
+    this.timesTables = timesTables;
     this.timeAllowed = timeAllowed;
-    this.startTime = new Date();
+    this.numberOfPlays = 0;
+    this.timesTable =
+      timesTables[Math.floor(Math.random() * this.timesTables.length)];
   }
 
   play(num) {
+    if (this.gameOver) {
+      return `${this.currentPlayer} has won!`;
+    }
+    if (this.numberOfPlays === 0) {
+      this.startTime = new Date();
+      this.numberOfPlays++;
+      return `It is ${this.currentPlayer}'s turn. Give me a multiple of ${this.timesTable}.`;
+    }
+    this.numberOfPlays++;
     this.endTime = new Date();
     const seconds = (this.endTime.getTime() - this.startTime.getTime()) / 1000;
     this.startTime = new Date();
 
-    if (this.gameOver) {
-      return `${this.currentPlayer} has won!`;
-    }
+    const nextTimesTable =
+      this.timesTables[Math.floor(Math.random() * this.timesTables.length)];
+
     if (seconds <= this.timeAllowed && num % this.timesTable === 0) {
       this.currentPlayer = this.next();
-      return `It is ${this.currentPlayer}'s turn.`;
+      this.timesTable = nextTimesTable;
+      return `It is ${this.currentPlayer}'s turn. Give me a multiple of ${this.timesTable}.`;
     }
     const deadPlayer = this.currentPlayer;
     this.gameOver = this.deletePlayer();
     this.currentPlayer = this.next();
+    this.timesTable = nextTimesTable;
     return this.gameOver
       ? `${this.currentPlayer} has won!`
-      : `${deadPlayer} has lost the game. It is ${this.currentPlayer}'s turn`;
+      : `${deadPlayer} has lost the game. It is ${this.currentPlayer}'s turn. Give me a multiple of ${this.timesTable}.`;
   }
 }
 
